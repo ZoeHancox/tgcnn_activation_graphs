06606c4303bca9640d6322a7a601698d53ff49e3
ZoeHancox <83767921+ZoeHancox@users.noreply.github.com>
2024-06-13 16:22:35 +0100

    leaky relu calc added

---
+++ src/calculations.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+# TODO TEST HERE FOR CHECKING WHETHER A FRACTIONAL DIVISION PRODUCES A CORRECT ARRAY
+
+def repeat_array_fractional(array, repeats):
+    """
+    Repeats the filter array a specified non-integer number of times.
+    This is because the graph may divide fractionally compared to the filter size.
+    
+    Parameters:
+    array (np.ndarray): The array to repeat.
+    repeats (float): The number of times to repeat the array.
+    
+    Returns:
+    np.ndarray: The repeated array.
+    """
+    # Separate the integer and fractional parts
+    int_part = int(repeats)
+    frac_part = repeats - int_part
+
+    # Repeat the array for the integer part
+    repeated_array_int = np.tile(array, (int_part, 1, 1))
+
+    # Calculate the number of rows to take for the fractional part
+    num_rows = array.shape[0]
+    frac_rows = int(num_rows * frac_part)
+
+    # Take the fractional part from the array
+    if frac_rows > 0:
+        repeated_array_frac = array[:frac_rows, :, :]
+        # Concatenate the integer part and the fractional part
+        result_array = np.concatenate((repeated_array_int, repeated_array_frac), axis=0)
+    else:
+        result_array = repeated_array_int
+
+    return result_array
+
+def leaky_relu(x, alpha=0.01):
+    return max(alpha * x, x) # if it's negative multiply by 0.01
