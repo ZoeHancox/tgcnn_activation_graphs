2fc1f7c914442d2b64d8c0f8be3150d9e427d396
ZoeHancox <83767921+ZoeHancox@users.noreply.github.com>
2024-06-14 17:52:21 +0100

    added element wise graph times filter code

--- src/calculations.py
+++ src/calculations.py
@@ -49,3 +49,40 @@ def leaky_relu(x:float, alpha:int):
         float: activation sum with leaky ReLU applied
     """
     return max(alpha * x, x) # if it's negative multiply by 0.01
+
+
+def get_act_graph_array(pat_graph: np.array, max_act_filt: np.array):
+    """Get the activated_graph which is the graph element-wise multiplied by the 'sliding window'. 
+
+    The stride must be the same as the filter.shape[0] so that slices don't overlap (otherwise we
+    get multiple mappings).
+
+    The filter is repeated to be the same length as the patient graph then the two graphs are 
+    multiplied together (element-wise) to get the activated graph.
+
+    Raises:
+        ValueError: if the patient graph is not 3D then raise an error.
+
+    Returns:
+        np.array: filter array .* patient array.
+    """
+
+    if pat_graph.ndim != 3:
+        raise ValueError("The input array must be 3-dimensional.")
+
+    vectorized_leaky_relu = np.vectorize(leaky_relu)
+    
+    filt_num_repeats = int(pat_graph.shape[0]/max_act_filt.shape[0])
+
+
+    # To reduce compute repeat the filter to be the same size as the patient graph.
+    # That way the two can be directly multiplied rather than sliding.
+    filt_repeated = repeat_array_fractional(max_act_filt, filt_num_repeats)
+
+    # Element-wise multiplication for the patient graph and the repeated filter
+    filt_times_graph = filt_repeated * pat_graph
+
+    # apply a leaky relu (activation)
+    activated_graph = vectorized_leaky_relu(filt_times_graph, 0.01)
+
+    return activated_graph
