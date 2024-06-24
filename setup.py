bf8c704e2fd3c01b6efc1458f767bc175a463525
ZoeHancox <83767921+ZoeHancox@users.noreply.github.com>
2024-06-24 19:00:08 +0100

    code to package to PyPI

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="tgcnn_act_graph",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        # 
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
