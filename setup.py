8d091e2449acb3b333fdda99f081a2bff1d4b87e
ZoeHancox <83767921+ZoeHancox@users.noreply.github.com>
2024-06-25 10:15:13 +0100

    install requirements added from requirements.txt

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     description = f.read()
 
+with open("requirements.txt", "r") as f:
+    install_requirements = f.read().splitlines()
+
 setup(
     name="tgcnn_act_graph",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
-    install_requires=[
-        # 
-    ],
+    install_requires=install_requirements,
     long_description=description,
     long_description_content_type="text/markdown",
 )
